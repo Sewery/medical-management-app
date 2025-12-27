b067f31a6a6b866dca6f3ee9ff5cfff04916417c
Seweryn Tasior <seweryn.tasior@op.pl>
2025-12-27 23:51:14 +0100

    Add a query in the JPA repository for finding available doctors and consulting rooms

--- backend-spring-boot/src/main/java/pl/edu/agh/to/backendspringboot/infrastructure/schedule/ScheduleRepository.java
+++ backend-spring-boot/src/main/java/pl/edu/agh/to/backendspringboot/infrastructure/schedule/ScheduleRepository.java
@@ -1,7 +1,39 @@
 package pl.edu.agh.to.backendspringboot.infrastructure.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pl.edu.agh.to.backendspringboot.domain.consulting_room.model.ConsultingRoomBrief;
+import pl.edu.agh.to.backendspringboot.domain.doctor.model.DoctorBrief;
 import pl.edu.agh.to.backendspringboot.domain.schedule.model.Schedule;
 
-public interface ScheduleRepository  extends JpaRepository<Schedule, Integer> {
+import java.time.LocalTime;
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+    @Query("""
+    SELECT d
+    FROM Doctor d
+    WHERE NOT EXISTS (
+        SELECT s
+        FROM Schedule s
+        WHERE s.doctor = d
+        AND s.shiftStart < :endTime
+        AND s.shiftEnd > :startTime
+    )
+    """)
+    List<DoctorBrief> findAvailableDoctorsInPeriod(LocalTime startTime, LocalTime endTime);
+
+    @Query("""
+    SELECT cr
+    FROM ConsultingRoom cr
+    WHERE NOT EXISTS (
+        SELECT s
+        FROM Schedule s
+        WHERE s.consultingRoom = cr
+        AND s.shiftStart < :endTime
+        AND s.shiftEnd > :startTime
+    )
+    """)
+    List<ConsultingRoomBrief> findAvailableConsultingRoomsInPeriod(LocalTime startTime, LocalTime endTime);
+
 }
